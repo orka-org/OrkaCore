6791f704f6c5b3680da8e8d113792743435263dd
Mhirii <ahmedmhiri218@gmail.com>
2025-04-27 18:43:29 +0100

    logging

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -6,6 +6,7 @@ import (
 	"github.com/orka-org/orkacore/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
@@ -15,6 +16,7 @@ func NewGRPCServer(c *conf.Server, auth *service.AuthServiceService, logger log.
 	opts := []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
+			logging.Server(logger),
 		),
 	}
 	if c.Grpc.Network != "" {
--- internal/server/http.go
+++ internal/server/http.go
@@ -6,6 +6,7 @@ import (
 	"github.com/orka-org/orkacore/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
@@ -15,6 +16,7 @@ func NewHTTPServer(c *conf.Server, auth *service.AuthServiceService, logger log.
 	opts := []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			logging.Server(logger),
 		),
 	}
 	if c.Http.Network != "" {
